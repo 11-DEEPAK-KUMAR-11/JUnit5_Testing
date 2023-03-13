0f12d003e14cd0cca0d83c5f68b79a239dd08cc7
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-03-13 16:39:20 +0530

    JUnit5 testing

---
+++ JUnit5_Testing/Test/com/masai/FirstJUnit5Test.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstJUnit5Test {
+
+	@Test
+	void test() {
+//		fail("Not yet implemented");
+	}
+
+}
